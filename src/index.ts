8501e23c58a7c660c3520067088bedb494198693
Filip Klimeš <filip@filipklimes.cz>
2024-12-26 15:05:25 +0100

    Add win condition

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const map = `
+const level1 = `
 ####################
 ##    []    []  []##
 ##            []  ##
@@ -10,24 +10,51 @@ const map = `
 ##        []      ##
 ####################`;
 
-let matrix: string[][] = [];
-let pos: number[] = [0, 0];
+const level2 = `
+########        
+###    #####    
+##         #    
+###  ###   #####
+#XX  XX# []@   #
+#      # [][]  #
+#XX    #       #
+################
+`;
 
-function resetMap() {
-  matrix = map
+const levels = [level1, level2];
+let currentLevel = 1;
+let winPositions: number[][] = [];
+
+let map: string[][] = [];
+let pos: number[] = [0, 0];
+let steps = 0;
+
+let youWinElement: HTMLElement = document.getElementById("you-win")!!;
+let stepsElement: HTMLElement = document.getElementById("steps")!!;
+
+function resetMap(input: string) {
+  map = input
     .trim()
     .split("\n")
     .map((x: string) => {
       return x.split("");
     });
 
-  matrix.forEach((row, x) => {
+  winPositions = [];
+  map.forEach((row, x) => {
     row.forEach((col, y) => {
       if (col == "@") {
         pos = [x, y];
       }
+      if (col == "X") {
+        winPositions.push([x, y]);
+      }
     });
   });
+
+  steps = 0;
+  youWinElement.classList.remove("show");
+  stepsElement.innerText = `steps: ${steps}`;
 }
 
 function findObject(
@@ -42,16 +69,16 @@ function findObject(
 
   let ans: any[][] = [];
   let [x, y] = pos;
-  switch (matrix[x][y]) {
+  switch (map[x][y]) {
     case "@":
-      ans.push([pos[0], pos[1], matrix[x][y]]);
+      ans.push([pos[0], pos[1], map[x][y]]);
       findObject([x + dir[0], y + dir[1]], dir, visited).forEach((res) =>
         ans.push(res),
       );
       break;
 
     case "[":
-      ans.push([pos[0], pos[1], matrix[x][y]]);
+      ans.push([pos[0], pos[1], map[x][y]]);
       if (JSON.stringify(dir) == "[0,-1]" || JSON.stringify(dir) == "[0,1]") {
         findObject([x + dir[0], y + dir[1]], dir, visited).forEach((res) =>
           ans.push(res),
@@ -65,7 +92,7 @@ function findObject(
       break;
 
     case "]":
-      ans.push([pos[0], pos[1], matrix[x][y]]);
+      ans.push([pos[0], pos[1], map[x][y]]);
       if (JSON.stringify(dir) == "[0,-1]" || JSON.stringify(dir) == "[0,1]") {
         findObject([x + dir[0], y + dir[1]], dir, visited).forEach((res) =>
           ans.push(res),
@@ -87,7 +114,7 @@ function findObject(
 
 function checkMove(obj: any[][], dir: number[]): boolean {
   return obj.every((pos) => {
-    return matrix[pos[0] + dir[0]][pos[1] + dir[1]] != "#";
+    return map[pos[0] + dir[0]][pos[1] + dir[1]] != "#";
   });
 }
 
@@ -97,7 +124,7 @@ function pushObject(obj: any[][], dir: number[]) {
     .slice()
     .reverse()
     .forEach((pos) => {
-      matrix[pos[0] + dir[0]][pos[1] + dir[1]] = pos[2];
+      map[pos[0] + dir[0]][pos[1] + dir[1]] = pos[2];
     });
 
   // Cleanup old positions
@@ -108,7 +135,7 @@ function pushObject(obj: any[][], dir: number[]) {
   obj
     .filter((pos) => !newPositions.has(JSON.stringify([pos[0], pos[1]])))
     .forEach((pos) => {
-      matrix[pos[0]][pos[1]] = " ";
+      map[pos[0]][pos[1]] = " ";
     });
 }
 
@@ -116,14 +143,31 @@ function movePlayer(dir: number[]) {
   let obj = findObject(pos, dir, new Set());
   if (checkMove(obj, dir)) {
     pushObject(obj, dir);
-    matrix[pos[0]][pos[1]] = " ";
+    map[pos[0]][pos[1]] = " ";
     pos = [pos[0] + dir[0], pos[1] + dir[1]];
+    steps += 1;
+    stepsElement.innerText = `steps: ${steps}`;
   }
 }
 
 let el: HTMLElement = document.getElementById("matrix")!!;
 function render(): void {
-  el.innerHTML = matrix
+  let filled = 0;
+
+  winPositions.forEach(([x, y]) => {
+    if (map[x][y] == " ") {
+      map[x][y] = "X";
+    } else if (map[x][y] != "X") {
+      filled += 1;
+    }
+  });
+
+  if (filled == winPositions.length) {
+    youWinElement.innerText = `You win in ${steps} steps`;
+    youWinElement.classList.add("show");
+  }
+
+  el.innerHTML = map
     .map((row) =>
       row
         .map((c) => {
@@ -134,6 +178,8 @@ function render(): void {
               return '<span class="text-50">#</span>';
             case "@":
               return '<span class="text-player">@</span>';
+            case "X":
+              return '<span class="text-target">X</span>';
             default:
               return c;
           }
@@ -165,7 +211,7 @@ window.addEventListener("keydown", (e) => {
   }
 
   if (e.key == "r") {
-    resetMap();
+    resetMap(levels[currentLevel]);
     render();
     return;
   }
@@ -201,9 +247,9 @@ rightButton.addEventListener("click", () => {
 
 let resetButton = document.getElementById("reset")!!;
 resetButton.addEventListener("click", () => {
-  resetMap();
+  resetMap(levels[currentLevel]);
   render();
 });
 
-resetMap();
+resetMap(levels[currentLevel]);
 render();
